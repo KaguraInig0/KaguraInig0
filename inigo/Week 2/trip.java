0c666d90f12ce4929ec487b2f2ba765f0677eba2
Inigo Dela Vega <143131551+KaguraInig0@users.noreply.github.com>
2023-09-05 18:29:40 -0700

    added trip program and made trip class

--- inigo/Week 2/trip.java
+++ inigo/Week 2/trip.java
@@ -3,18 +3,34 @@ public class trip
 {
 	public static void main (String [] args)
 	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("This is the 1st package:");
+		System.out.println("*******************************************");
+		int party = 0;
+		double totalCharges = 0.0;
+		double baseChargeRate = 350.0;
+		double trainingChargesRate = 100.0;
+		double discountRate = .10;
+		double depositRate = .50;
+		double totalRentalCharges = 0.0;
+ 		double totalRentalTents= 40.0; //group of 4
+		int days = 0;
+		int	numberOfTents = 0;
+		double baseCharges = 0.0;
+		double trainingCharges = 0.0;
+		int nights = days-1;
+		System.out.println("How large is your party?");
+		party = input.nextInt();
+		
+		
+		
 	
 	
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
+		System.out.println("********************************* outputs *********************************");
+		System.out.println("The party size is:" + party);
 	
 	}
 
